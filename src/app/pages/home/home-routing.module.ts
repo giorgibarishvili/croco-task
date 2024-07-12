501b830382019631ed3aca9016d809d54ae4e886
Giorgi Barishvili <90037916+giorgibarishvili@users.noreply.github.com>
2024-07-12 22:20:58 +0400

    Init commit

---
+++ src/app/pages/home/home-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+const routes: Route[] = [
+  {
+    path:'',
+    pathMatch: 'full',
+    component: HomeComponent
+  }
+]
+ 
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class HomeRoutingModule { }
